a0a85ba4b16d5aec5c97e083f51c8d45bc9c1c7f
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2022-03-22 19:00:03 -0400

    Update pinning

--- config/settings/test.py
+++ config/settings/test.py
@@ -24,6 +24,3 @@ PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
-
-# Your stuff...
-# ------------------------------------------------------------------------------
