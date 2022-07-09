8a45931eea874e22c731a2d8ef0615e8e3207498
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2022-07-08 22:04:54 -0400

    test(stripe): Add stripe mock

--- poc_django_stripe/users/migrations/0003_alter_user_id.py
+++ poc_django_stripe/users/migrations/0003_alter_user_id.py
@@ -6,13 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0002_alter_user_id'),
+        ("users", "0002_alter_user_id"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='user',
-            name='id',
-            field=models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+            model_name="user",
+            name="id",
+            field=models.AutoField(
+                auto_created=True,
+                primary_key=True,
+                serialize=False,
+                verbose_name="ID",
+            ),
         ),
     ]
